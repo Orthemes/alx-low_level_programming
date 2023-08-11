38cef47a34322dad0b50a73879d8a02c47745ae5
Orthemes <62575677+Orthemes@users.noreply.github.com>
2023-08-10 20:08:29 -0700

    Push

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desciption: A C program that prints exactly
+ * "with proper grammar, but the outcome is a piece of art,",
+ * followed by a new line.
+ *
+ * Return: 0 when exiting
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
