bc032dee9de2491f5de596f87ec9da97c2cf66d3
Orthemes <62575677+Orthemes@users.noreply.github.com>
2023-08-10 19:57:04 -0700

    Push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #inclue <stdio.h>
 
-/**
- * A program that prints exactly a text,
- * followed by a new line.
- */
+/* A program */
 int main(void){
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
