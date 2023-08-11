090ff12a97e29c8136f829e0295482eee6d3c6b8
Orthemes <62575677+Orthemes@users.noreply.github.com>
2023-08-10 20:05:26 -0700

    Push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-/* A program */
-int main(void){
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints exactly
+ * "Programming is like building a multilingual puzzle",
+ * followed by a new line.
+ *
+ * Return: 0 when exiting
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
